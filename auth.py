d51a01ec92d8ad5d9ebc476f0f66d0c7e9d0b53e
LETTING <la@Lettings-MacBook-Pro.local>
2024-11-09 11:20:10 +0300

    refresh_token

--- auth.py
+++ auth.py
@@ -5,10 +5,13 @@ from fastapi import Depends,HTTPException
 from datetime import datetime,timedelta,timezone
 from fastapi.security import HTTPAuthorizationCredentials,HTTPBearer
 from jose import jwt,ExpiredSignatureError,JWTError
+import uuid
 
 SECRET_KEY ='1kslll3o3l'
 ALGORITHM ='HS256'
 ACCESS_TOKEN_EXPIRY_TIME = 30
+# ACCESS_TOKEN_EXPIRY_TIME = 3600
+
 
 pwd_context = CryptContext(schemes=['bcrypt'],deprecated="auto")
 
@@ -25,7 +28,7 @@ def create_token(data:dict,expries_delta:timedelta | None=None):
         expires = datetime.now(timezone.utc) + expries_delta(minutes=30)
     to_encode.update({"exp":expires})
     encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
-    print(encoded_jwt)
+
     return encoded_jwt
 
 def get_token_auth_heaaders(credentials:HTTPAuthorizationCredentials=Depends(HTTPBearer())):
@@ -35,19 +38,20 @@ def get_token_auth_heaaders(credentials:HTTPAuthorizationCredentials=Depends(HTT
     return credentials.credentials
     
 
-def get_current_user(token:str=Depends(get_token_auth_heaaders)):
+async def get_current_user(token: str = Depends(get_token_auth_heaaders)):
     try:
-        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get('user')
-        print(email)
         if email is None:
-            raise HTTPException(status_code=401,detail="Could not validate credentials")
-        print(email)
+            raise HTTPException(status_code=401, detail="Could not validate credentials")
     except ExpiredSignatureError:
-        raise HTTPException(status_code=401,detail="Token already expired")
+        raise HTTPException(status_code=401, detail="Token has expired")
     except JWTError:
-        raise HTTPException(status_code=401,detail="Invalid token")
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
     user = check_user(email)
     if not user:
-        raise HTTPException(status_code=401,detail="User does not exist")
+        raise HTTPException(status_code=401, detail="User does not exist")
+    
     return user
+
